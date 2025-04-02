75986d782f94c854ed3fa8926dba79d582b1f678
Max Mansfield <max.mansfield@zoom.us>
2025-04-01 21:00:27 -0600

    updated webhook schema

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 import rtms from "@zoom/rtms";
 
 
-const options = {
-    meeting_uuid: "anything-goes-here", 
-    rtms_stream_id: "and-here-too", 
-    server_urls: "wss://tptestsvr122.zoomdev.us:9443"
-}
-
-rtms.onAudioData((data, timestamp, metadata) => {
-    console.log(data);
-});
-
-rtms.join(options);
-
-
 // The below method works if you want to trigger the test server without a webhook
-
-/* rtms.onWebhookEvent(({eventType, payload}) => {
-    if (eventType !== "meeting.rtms_started") 
+rtms.onWebhookEvent(({event, payload}) => {
+    console.log(event, payload)
+ 
+    if (event !== "meeting.rtms_started") 
         return
 
     const client = new rtms.Client()
@@ -26,7 +14,7 @@ rtms.join(options);
         console.log(data)
     });
 
-    console.log(payload)
+
 
     client.join(payload)
-});*/
+});
